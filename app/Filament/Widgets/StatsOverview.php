3c81f751725edcb7479844e6c703e6da8bde0b7a
NVdty <nadity6@gmail.com>
2024-10-31 21:40:06 +0700

    filter date

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,16 +2,29 @@
 
 namespace App\Filament\Widgets;
 
+
 use App\Models\Transaction;
+use Illuminate\Support\Carbon;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
     protected function getStats(): array
     {
-        $income = Transaction::incomes()->get()->sum('amount');
-        $expense = Transaction::expenses()->get()->sum('amount');
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+             
+
+        $income = Transaction::incomes()->get()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
+        $expense = Transaction::expenses()->get()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
         $difference = $income - $expense;
 
         return [
